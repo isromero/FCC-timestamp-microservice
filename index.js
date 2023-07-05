004a655e87856f96c5c01624351281d5c9701c58
Replit user <>
2023-07-05 07:42:16 +0000

    done

--- index.js
+++ index.js
@@ -20,8 +20,31 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  const inputDate = req.params.date;
+  let date;
+  if (!isNaN(inputDate)) {
+    date = new Date(parseInt(inputDate));
+  } else {
+    date = new Date(inputDate);
+  }
+  if (isNaN(date)) {
+    res.json({
+      "error": "Invalid Date"
+    });
+  } else {
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    });
+  }
+});
+
+app.get("/api", function (req, res) {
+  res.json({
+      "unix": new Date().getTime(),
+      "utc": new Date().toUTCString()
+  });
 });
 
 
